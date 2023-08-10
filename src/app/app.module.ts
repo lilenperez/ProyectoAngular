5b38234747d4a2228f3690e591777be7ddfa0b61
Natalí Perez <135169458+lilenperez@users.noreply.github.com>
2023-08-09 23:01:21 -0300

    componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,32 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { IntroSectionComponent } from './intro-section/intro-section.component';
+import { AboutUsSectionComponent } from './about-us-section/about-us-section.component';
+import { ServicesSectionComponent } from './services-section/services-section.component';
+import { WhyUsSectionComponent } from './why-us-section/why-us-section.component';
+import { PortfolioSectionComponent } from './portfolio-section/portfolio-section.component';
+import { ClientsSectionComponent } from './clients-section/clients-section.component';
+import { TeamSectionComponent } from './team-section/team-section.component';
+import { ContactSectionComponent } from './contact-section/contact-section.component';
+import { FooterComponent } from './footer/footer.component';
+import { TestimonialsComponent } from './testimonials/testimonials.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    IntroSectionComponent,
+    AboutUsSectionComponent,
+    ServicesSectionComponent,
+    WhyUsSectionComponent,
+    PortfolioSectionComponent,
+    ClientsSectionComponent,
+    TeamSectionComponent,
+    ContactSectionComponent,
+    FooterComponent,
+    TestimonialsComponent,
   ],
   imports: [
     BrowserModule,
---
+++ src/app/services-section/services-section.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { BoxComponent } from './box/box.component';
+
+@Component({
+  selector: 'app-services-section',
+  templateUrl: './services-section.component.html',
+  styleUrls: ['./services-section.component.css']
+})
+export class ServicesSectionComponent {
+
+}
